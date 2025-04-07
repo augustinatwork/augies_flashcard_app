a3b207b7abcbf0b2c07e76e15bbd8c57e2cae2cd
augustinatwork <augustinatwork@gmail.com>
2025-04-07 20:20:15 +0530

    first commit by augie
    writing it up

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Replace with your Firebase project settings
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "your-project.firebaseapp.com",
+    databaseURL: "https://your-project.firebaseio.com",
+    projectId: "your-project",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "XXXXXXX",
+    appId: "YOUR_APP_ID"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
